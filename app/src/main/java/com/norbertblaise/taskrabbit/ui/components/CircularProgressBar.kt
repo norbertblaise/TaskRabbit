5290d78d9c6561cc867f62f0c5b5fff5de14f86d
norbertblaise <lukwayi@gmail.com>
2022-09-08 21:41:17 +0300

    theming added

--- app/src/main/java/com/norbertblaise/taskrabbit/ui/components/CircularProgressBar.kt
+++ app/src/main/java/com/norbertblaise/taskrabbit/ui/components/CircularProgressBar.kt
@@ -2,26 +2,34 @@ package com.norbertblaise.taskrabbit.ui.components
 
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PaintingStyle.Companion.Stroke
+
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.norbertblaise.taskrabbit.ui.theme.Grey
+import com.norbertblaise.taskrabbit.ui.theme.Salmon500
 
 @Composable
 fun CircularProgressBar(
     percentage: Float,
-    radius: Dp = 50.dp,
+    label: String,
+    currentPom: String,
+    numPoms: String,
+    radius: Dp = 260.dp,
     color: Color,
 
     ) {
@@ -30,20 +38,57 @@ fun CircularProgressBar(
         animationSpec = tween(durationMillis = 1000, delayMillis = 0)
     )
     Box(
+        contentAlignment = Alignment.Center,
         modifier = Modifier.size(
             radius * 2f
         )
     ) {
-        androidx.compose.foundation.Canvas(modifier = Modifier.fillMaxSize(), onDraw = {
-            drawArc(
-                color = color,
-                startAngle = -90f,
-                sweepAngle = 360 * currPercentage.value,
-                useCenter = false,
-                style = Stroke(36.dp.toPx(), cap = StrokeCap.Round)
+        androidx.compose.foundation.Canvas(
+            modifier = Modifier.size(radius * 2f),
+            onDraw = {
+                drawCircle(
+                    color = Grey,
+                    style = Stroke(38.dp.toPx())
+                )
+                drawArc(
+                    color = color,
+                    startAngle = -90f,
+                    sweepAngle = 360 * currPercentage.value,
+                    useCenter = false,
+                    style = Stroke(38.dp.toPx(), cap = StrokeCap.Round)
+                )
+            }
+        )
+
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                text = label,
+                style = MaterialTheme.typography.h3,
+
+                )
+            Text(
+                text = currPercentage.value.toString(),
+                style = MaterialTheme.typography.h1
+            )
+            Text(
+                "$currentPom of $numPoms",
+                style = TextStyle(
+                    fontSize = 24.sp
+                )
             )
         }
-        )
-        Text(text = currPercentage.value.toString())
     }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun CircularProgressBarPreview() {
+    CircularProgressBar(
+        percentage = 0.5f,
+        label = "Focus",
+        currentPom = "2",
+        numPoms = "4",
+        color = Salmon500
+    )
 }
--- app/src/main/java/com/norbertblaise/taskrabbit/ui/theme/Type.kt
+++ app/src/main/java/com/norbertblaise/taskrabbit/ui/theme/Type.kt
@@ -2,9 +2,27 @@ package com.norbertblaise.taskrabbit.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.norbertblaise.taskrabbit.R
+
+val NunitoFontFamily = FontFamily(
+    fonts = listOf(
+        Font(
+            resId = R.font.nunito_medium,
+            weight = FontWeight.Medium,
+            style = FontStyle.Normal
+        ),
+        Font(
+            resId = R.font.nunito_bold,
+            weight = FontWeight.Bold,
+            style = FontStyle.Normal
+        )
+    )
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -14,7 +32,19 @@ val Typography = Typography(
         fontSize = 16.sp
     ),
     h1 = TextStyle(
-
+        fontFamily = NunitoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 48.sp
+    ),
+    h2 = TextStyle(
+        fontFamily = NunitoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 36.sp
+    ),
+    h3 = TextStyle(
+        fontFamily = NunitoFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 28.sp
     )
     /* Other default text styles to override
     button = TextStyle(
